8aab882788193d147ff2a52b56127b7877841338
quyet <code.maito@gmail.com>
2022-10-25 16:39:24 +0700

    tested deployment

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-envsubst /etc/nginx/default.conf.tpl > etc/nginx.conf.d/default.conf
-nginx -g 'deamon off;'
+set -e
+
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
