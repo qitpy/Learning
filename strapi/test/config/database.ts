c4438d72cb0d3aa1d1856115ea671f104b0da679
quyet <code.maito@gmail.com>
2023-01-02 16:26:17 +0700

    strapi | learning

---
+++ strapi/test/config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'rds-uc-terraform-dev-crawling-v1.cheuetgkxqpo.us-west-2.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'uct_crawling_v1'),
+      user: env('DATABASE_USERNAME', 'uct_crawling_v1_root'),
+      password: env('DATABASE_PASSWORD', 'ihvgXRxKbcZVCaPP'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+  },
+});
