4b0a9bd955b2f5dae4fa7a53910df7fa0f7a92f7
quyet <code.maito@gmail.com>
2023-02-01 10:36:53 +0700

    learning fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -177,3 +177,34 @@ async def get_portal(teleport: bool = False) -> Response:
     if teleport:
         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     return {"message": "Here's your interdimensional portal."}
+
+# Exception
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+
+items = {"foo": "The Foo Wrestlers"}
+
+@app.get("/exception/{item_id}")
+async def raise_exception(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
+
+# exception global handler
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
